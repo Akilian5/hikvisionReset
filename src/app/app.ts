7d751aac5696e7c27b4a521c381756babedd5dd1
Nicolás García <akilian182@gmail.com>
2025-08-01 19:18:54 -0500

    En este commit corregimos el bug de las imagenes del carrusel, hicimos el navbar funcional, creamos un footer con enlaces rapidos y contacto y informacion de terminos y condiciones. Codigo limpio sin errores y bugs hasta la fecha

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-// src/app/app.component.ts
+
 import { Component, signal } from '@angular/core';
 
 import { InicioComponent } from './components/inicio/inicio';
@@ -7,20 +7,17 @@ import { FuncionaComponent } from './components/funciona/funciona';
 import { ContactoComponent } from './components/contacto/contacto';
 import { NavbarComponent } from './components/navbar/navbar';
 import { FooterComponent } from './components/footer/footer';
-import { RouterOutlet } from '@angular/router'; // ✅ Import correcto
+import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    InicioComponent,
-    ProductosComponent,
-    FuncionaComponent,
-    ContactoComponent,
+
     NavbarComponent,
     FooterComponent,
-    RouterOutlet // ✅ Aquí ya funciona
-  ],
+    RouterOutlet 
+],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
